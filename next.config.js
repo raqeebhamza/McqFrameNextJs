ada54acbe3fb19769079b186651ab33aa85b04a1
Raqeeb Hamza <raqeeb.hamza@funavry.com>
2022-03-01 18:16:47 +0500

    created the frame for the MCQ

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 const nextConfig = {
   reactStrictMode: true,
+  i18n:{
+    locales:['en','fr'],
+    defaultLocale:'en'
+  }, 
 }
-
 module.exports = nextConfig
